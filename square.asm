b5400b03510135246b55b50a3dd034cbdb21b64a
manish556 <2015csb1019@iitrpr.ac.in>
2016-09-14 22:45:28 +0530

    Update square.asm

--- square.asm
+++ square.asm
@@ -3,13 +3,13 @@
 	mov r3, 0     // r3 is zero if not perfect square , 1  if it is
 .start:
 	mov r1,0      // r1 is counter
-loop:	add r1,r1,1   // incrementation
+.loop:	add r1,r1,1   // incrementation
 	mul r2,r1,r1   // r2 is temp var saving square of counter
 	cmp r2,r0       // compare counter square and given number
 	beq .equal     // if they are equal make r3 1
 	cmp r2,r0       // compare counter square and given number
 	bgt .return     // if equal go to return
-	b loop          // repeat the process
+	b .loop          // repeat the process
 .equal:
 	mov r3,1       // make r3 1 (when perfect square)
 .return:
