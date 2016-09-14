c2719ae70f6e57a6dfa60fd556fc28ddff5b4143
manish556 <2015csb1019@iitrpr.ac.in>
2016-09-14 22:43:16 +0530

    Update gcd.asm

--- gcd.asm
+++ gcd.asm
@@ -13,14 +13,14 @@
 
 .start:
 
-loop:
+.loop:
 	cmp r1,0        //  compare if one number(r1) becomes zero
 	beq .gcd        // if true the other number (r0) becomes gcd, go to label gcd and print r0
 	mov r2,r1       // save r1 in temp register r2
 	mod r3,r0,r1    // save r0 mod r1 in temp register r3
 	mov r1,r3       // save r3 (r0 mod r1) in r1
-	mov r0,r2;      // save r2 (initial value of r1 of current iteration) in r0
-	b loop          // repeat the loop
+	mov r0,r2      // save r2 (initial value of r1 of current iteration) in r0
+	b .loop          // repeat the loop
 
 .gcd:
 	.print r0       // r0 is our gcd, so print
